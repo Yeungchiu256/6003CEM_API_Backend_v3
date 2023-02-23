81693b44ccfc008fdc027f144f6cf306d9b11c76
Yeungchiu256 <122268615+Yeungchiu256@users.noreply.github.com>
2023-02-23 07:21:48 +0000

    createArticle and updateArticle complete

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 //npm i koa koa-bodyparser koa-json koa-logger koa-router @types/koa @types/koa-bodyparser @types/koa-json @types/koa-logger @types/koa-router
 
 import Koa from "koa";
-import Router from "koa-router";
+import Router, { RouterContext } from "koa-router";
 import logger from "koa-logger";
 import json from "koa-json";
-import {router as articles} from "./routes/articles";
+import { router as articles } from "./routes/articles";
 
 
 const app: Koa = new Koa();
 const router: Router = new Router();
 
-const welcomeAPI = async(ctx: RouterContext, next: any)=>{
-  ctx.body = {message: "Welcome to the blog API!"};
+const welcomeAPI = async (ctx: RouterContext, next: any) => {
+  ctx.body = { message: "Welcome to the blog API!" };
   await next();
 }
 
@@ -21,6 +21,17 @@ app.use(logger());
 app.use(json());
 app.use(router.routes());
 app.use(articles.routes());
+app.use(async (ctx: RouterContext, next: any) => {
+  try {
+    await next();
+    if (ctx.status === 404) {
+      ctx.status = 404;
+      ctx.body = { err: "Resource not found" };
+    } 
+  } catch (err: any) {
+      ct.body = { err: err }
+  }
+});
 app.listen(10888);
 
 
--- routes/articles.ts
+++ routes/articles.ts
@@ -1,45 +1,67 @@
-import Router, {RouterContext} from "koa-router";
+import Router, { RouterContext } from "koa-router";
 import bodyParser from "koa-bodyparser";
 
 const articles = [
-  {title:'hello article', fullText:'some text here to fill the body'},
-  {title:'another article', fullText:'again here is some text here to fill'},
-  {title:'coventry university ', fullText:'some news about coventry university'},
-  {title:'smart campus', fullText:'smart campus is coming to IVE'} ];
+  { title: 'hello article', fullText: 'some text here to fill the body' },
+  { title: 'another article', fullText: 'again here is some text here to fill' },
+  { title: 'coventry university ', fullText: 'some news about coventry university' },
+  { title: 'smart campus', fullText: 'smart campus is coming to IVE' }];
 
 
-const router = new Router({prefix:'/api/v1/articles'});
+const router = new Router({ prefix: '/api/v1/articles' });
 
-const getAll = async (ctx: RouterContext, next: any)=>{
+const getAll = async (ctx: RouterContext, next: any) => {
   ctx.body = articles;
   await next();
 }
 
-//const getAll = async (ctx:RouterContent, next: any)=>{};
-const getById = async (ctx: RouterContext, next: any)=>{
-  let id = +ctx.params.id
-  if ((id < articles.length+1) && (id > 0)) {
-    ctx.body = articles[id-1];
+
+const getById = async (ctx: RouterContext, next: any) => {
+  let id = +ctx.params.id;
+  if ((id < articles.length + 1) && (id > 0)) {
+    ctx.body = articles[id - 1];
   } else {
     ctx.status = 404;
   }
   await next();
 }
-const createArticle = async (ctx:RouterContext, next: any)=>{
-  let {title, fullText} =ctx.request.body;
-  let newArticle = {title: title, fullText: fullText};
+const createArticle = async (ctx: RouterContext, next: any) => {
+  let { title, fullText } = ctx.request.body;
+  let newArticle = { title: title, fullText: fullText };
   articles.push(newArticle);
   ctx.status = 201;
   ctx.body = newArticle;
   await next();
 };
-const updateArticle = async (ctx:RouterContext, next: any)=>{};
-const deleteArticle = async (ctx:RouterContext, next: any)=>{};
+const updateArticle = async (ctx: RouterContext, next: any) => {
+  let id = +ctx.params.id;  // read the value of ID
+  let { title, fullText } = ctx.request.body;
+  if ((id < articles.length + 1) && (id > 0)) {
+    articles[id - 1].title = title;
+    articles[id - 1].fullText = fullText;
+    ctx.status = 200;
+    ctx.body = articles;
+  } else {
+    ctx.status = 404;
+  }
+  await next();
+};
+const deleteArticle = async (ctx: RouterContext, next: any) => {
+  let id = +ctx.params.id;
+  if ((id < articles.length + 1) && (id > 0)) {
+    articles.splice(id - 1, 1);
+    ctx.status = 200;
+    ctx.body = articles;
+  } else {
+    ctx.status = 404;
+  }
+  await next();
+};
 
 router.get('/', getAll);
 router.post('/', bodyParser(), createArticle);
 router.get('/:id([0-9]{1,})', getById);
-router.put('/:id([0-9]{1,})', updateArticle);
+router.put('/:id([0-9]{1,})', bodyParser(), updateArticle);
 router.del('/:id([0-9]{1,})', deleteArticle);
 
 export { router };
