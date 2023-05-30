db587cef791d396338ab5d09723bf228f5b3e2e1
Replit user <>
2023-05-30 15:28:32 +0000

    new repositories: new endpoint of image upload and change koa-passport setting

---
+++ api.ts
@@ -0,0 +1,5 @@
+// store db information
+//  https://api.elephantsql.com/console/1c98702a-6b16-4ea8-b068-541ce6600173/details
+export const config = {
+  url: 'https://6003cemapibackend.yeungchiu256.repl.co', /* Backend Path */
+}
